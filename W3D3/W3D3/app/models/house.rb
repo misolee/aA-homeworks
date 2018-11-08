8dda2eafd0826de490f5406321dd4e53798678ca
Miso Lee <mzxmiso@gmail.com>
2018-11-07 21:17:57 -0800

    Add W3D3 HW

--- W3D3/W3D3/app/models/house.rb
+++ W3D3/W3D3/app/models/house.rb
@@ -9,6 +9,8 @@
 #
 
 class House < ActiveRecord::Base
+  validates :address, presence: true
+  
   has_many :residents,
     primary_key: :id,
     foreign_key: :house_id,
--- W3D3/W3D3/app/models/person.rb
+++ W3D3/W3D3/app/models/person.rb
@@ -10,6 +10,8 @@
 #
 
 class Person < ActiveRecord::Base
+  validates :name, presence: true
+
   belongs_to :house,
     primary_key: :id,
     foreign_key: :house_id,
